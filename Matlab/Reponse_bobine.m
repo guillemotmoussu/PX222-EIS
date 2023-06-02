8acfa887071dda8e54c7c2e803366c3e216b8699
Guillemot Moussu <guillemot.moussu@gmail.com>
2023-06-02 11:18:29 +0200

    Séance 4

--- Matlab/Reponse_bobine.m
+++ Matlab/Reponse_bobine.m
@@ -2,14 +2,15 @@ clear
 close all
 clc
 
-I0 = 2;
-Z0 = 22 * 10^-3;
-L1 = 6.73;
-alpha = 2.06;
+I0 = 1.8;
+Z0 = 6.2 * 10^-3;
+L1 = 1.296;
+m = 37.6;
+alpha = L1 / (2 * (45.8215)^2 * m * 9.81);
+alpha2 = 1/1.0243;
 
 ki = (I0 * L1 * alpha) / (1 + alpha * Z0) ^2;
 kz = -(I0 ^2 * L1 * alpha ^2) / (1 + alpha * Z0) ^3;
-m = 35.8;
 
 num =  ki ;
 den = [(-m) 0 kz];
