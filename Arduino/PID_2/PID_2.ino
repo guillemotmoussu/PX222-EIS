7e07317e1071130e28ff559df6ddb6384e9ebb5d
Guillemot Moussu <guillemot.moussu@gmail.com>
2023-06-08 10:32:46 +0200

    Dernière séance

--- Arduino/PID_2/PID_2.ino
+++ Arduino/PID_2/PID_2.ino
@@ -9,30 +9,35 @@ PID myPID(&Entree, &Sortie, &Consigne, Kp, Ki, Kd, DIRECT);
 
 void setup()
 {
+  Serial.begin(9600);
   Consigne = map(analogRead(0), 0, 1023, 0, 255) + 15;
 
-  Kp = (map(analogRead(1), 0, 1023, 1000, 0));
-  Ki = (map(analogRead(2), 0, 1023, 1000, 0));
-  Kd = (map(analogRead(3), 0, 1023, 1000, 0));
+  Kp = (map(analogRead(1), 0, 1023, 5000, 0));
+  Ki = (map(analogRead(2), 0, 1023, 2000, 0));
+  Kd = (map(analogRead(3), 0, 1023, 5000, 0));
 
-  Kp = Kp/100;
-  Ki = Ki/100;
-  Kd = Kd/100;
+  Kp = Kp/1000;
+  Ki = Ki/1000;
+  Kd = Kd/1000;
 
   myPID.SetTunings(Kp, Ki, Kd);  
 
   myPID.SetMode(AUTOMATIC);
+
+  Serial.println(Kp);
+  Serial.println(Ki);
+  Serial.println(Kd);
 }
 
 void loop()
 {
-  Kp = (map(analogRead(1), 0, 1023, 1000, 0));
+  Kp = (map(analogRead(1), 0, 1023, 5000, 0));
   Ki = (map(analogRead(2), 0, 1023, 1000, 0));
-  Kd = (map(analogRead(3), 0, 1023, 1000, 0));
+  Kd = (map(analogRead(3), 0, 1023, 5000, 0));
 
-  Kp = Kp/100;
-  Ki = Ki/100;
-  Kd = Kd/100;
+  Kp = Kp/1000;
+  Ki = Ki/10;
+  Kd = Kd/1000;
 
   myPID.SetTunings(Kp, Ki, Kd);  
 
